ce62edf79fe7c14f487f5d12b3dbbdd2942439e3
ChiragAgg5k <110609663+ChiragAgg5k@users.noreply.github.com>
2023-11-04 13:57:19 +0530

    removed t3 stack boilerplate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
 import "~/styles/globals.css";
 
-import { Inter } from "next/font/google";
 import { headers } from "next/headers";
-
+import { GeistSans } from 'geist/font'
 import { TRPCReactProvider } from "~/trpc/react";
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 export const metadata = {
   title: "Create T3 App",
@@ -22,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
+    <html lang="en" suppressHydrationWarning className={GeistSans.className}>
+      <body>
         <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
       </body>
     </html>
