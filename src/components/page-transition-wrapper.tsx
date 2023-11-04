98293caa1c035f2553111ff4abff1f5a8751b6e0
ChiragAgg5k <110609663+ChiragAgg5k@users.noreply.github.com>
2023-11-04 15:47:17 +0530

    formatting

--- src/components/page-transition-wrapper.tsx
+++ src/components/page-transition-wrapper.tsx
@@ -5,19 +5,19 @@ import { motion } from "framer-motion"; // Fade in and out animation for page tr
 // Fade in and out animation for page transitions
 
 export const PageWrapper = ({
-                                children,
-                                className,
-                            }: {
-    children: React.ReactNode;
-    className?: string;
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
 }) => (
-    <motion.div
-        className={className}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.5 }}
-    >
-        {children}
-    </motion.div>
+  <motion.div
+    className={className}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.5 }}
+  >
+    {children}
+  </motion.div>
 );
