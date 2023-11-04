8655aef302fe53c07b6ae3332ae7f0357faa3501
ChiragAgg5k <110609663+ChiragAgg5k@users.noreply.github.com>
2023-11-04 15:12:40 +0530

    initialized shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { headers } from "next/headers";
 import { GeistSans } from 'geist/font'
 import { TRPCReactProvider } from "~/trpc/react";
+import {ThemeProvider} from "~/components/theme-provider";
 
 export const metadata = {
   title: "Create T3 App",
@@ -18,7 +19,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={GeistSans.className}>
       <body>
-        <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
+        <TRPCReactProvider headers={headers()}>
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+          >
+          {children}
+          </ThemeProvider>
+          </TRPCReactProvider>
       </body>
     </html>
   );
